88d533b28552bae24cab5cec1f20d3e39e3384bf
Andrew Kihs <ajkihs@gmail.com>
2021-04-20 16:20:24 -0400

    Finish asteroid movement

--- src/index.js
+++ src/index.js
@@ -27,22 +27,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const g = new Game();
     const gameview = new GameView(g, ctx)
     gameview.start();
-    // const mo = new MovingObject({
-    //     pos: [30, 30],
-    //     vel: [10, 10],
-    //     radius: 5,
-    //     color: "#00FF00"
-    // });
-
-    // mo.draw(ctx);
-    // mo.move();
-    // mo.draw(ctx);
-
-    // const asteroid = new Asteroid([250,250]);
-    // console.log(asteroid);
-    // asteroid.draw(ctx);
-
-    // const g = new Game();
 
 })
 
